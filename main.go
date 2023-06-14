5c6317f8993d372f9d7b8195802abbe659f7b095
AriadnaLlB <ariadna.lb@outlook.com>
2023-06-14 19:32:13 +0200

    new functions

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func suma(a, b int) int {
+	return a + b
+}
+
+func resta(a, b int) int {
+	return a - b
+}
+
+func main() {
+	resultadoSuma := suma(2, 2)
+	fmt.Println("El resultado de la suma es:", resultadoSuma)
+
+	resultadoResta := resta(4, 5)
+	fmt.Println("El resultado de la resta es:", resultadoResta)
 }
